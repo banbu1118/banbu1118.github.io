c65e96e215a14a2d7c62f2a6f05c52564f2ae816
banbu1118 <74441338+banbu1118@users.noreply.github.com>
2025-09-16 19:51:05 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -155,13 +155,20 @@ const config = {
         theme: { light: 'default', dark: 'dark' },
       },
     }),
+  // stylesheets: [
+  //   {
+  //     href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+  //     type: 'text/css',
+  //     integrity:
+  //       'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+  //     crossorigin: 'anonymous',
+  //   },
+  // ],
+
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      href: 'katex.min.css', // 对应 static 文件夹的路径
       type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
     },
   ],
 };
